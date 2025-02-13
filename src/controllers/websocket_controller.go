7d925dacbc2aec06b1a0b1f66df1fa8b90d4b9d7
Arnold Molenaar <arnoldp1991@gmail.com>
2025-02-13 14:14:52 +0100

    📚 docs: Adding explenation of when the websocket closes
    Changelog: documentation

--- src/controllers/websocket_controller.go
+++ src/controllers/websocket_controller.go
@@ -17,6 +17,8 @@ func WebSocketProgress(c *websocket.Conn) {
 	for {
 		// Keep the connection open
 		if _, _, err := c.ReadMessage(); err != nil {
+			// When the ReadMessage method returns an error (e.g., when the client closes their browser),
+			// the loop breaks, and the function returns.
 			break
 		}
 	}
